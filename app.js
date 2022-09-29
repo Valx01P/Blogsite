2a1e5339f31b6cd0d746e8dc7f792fc64fd3634b
Pablo Valdes <pablovaldes0925@gmail.com>
2022-09-29 00:51:56 -0400

    more page routes

--- app.js
+++ app.js
@@ -32,7 +32,17 @@ app.get("/about", function(req, res) {
   res.render("about", {aboutContent: aboutContent});
 });
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
 
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  
+});
 
 
 
